cc54b1ec5ce594d6681b3717653c8eb0b24f874e
Tim Crossley <tcrossley@gmail.com>
2022-12-02 07:46:19 -0800

    Day 2 solution

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::time::Instant;
 
 fn main() {
     day1();
+    day2();
 }
 
 pub fn day1() {
@@ -25,5 +26,39 @@ pub fn day1() {
     println!("Day 1");
     println!("  Part 1: {}", a);
     println!("  Part 2: {}", a+b+c);
+    println!("  Elapsed time: {:?}", elapsed);
+}
+
+fn day2() {
+    let input = include_str!("../data/day2.txt");
+    let start = Instant::now();
+
+    let calc_score1 = |game: &str| {
+        let b = game.as_bytes();
+        // opponents play as 0,1,2
+        let opp = (b[0] - b'A') as i32;
+        // my play as 0,1,2
+        let my = (b[2] - b'X') as i32;
+        if (my+1)%3 == opp { 0+my+1 }
+        else if my == opp { 3+my+1 }
+        else { 6+my+1 }
+    };
+    let calc_score2 = |game: &str| {
+        let b = game.as_bytes();
+        // opponents play as 0,1,2
+        let opp = (b[0] - b'A') as i32;
+        // my strat as 0,1,2
+        let strat = (b[2] - b'X') as i32;
+        if strat == 0 { 0+(opp+2)%3+1 }
+        else if strat == 1 { 3+opp+1 }
+        else { 6+(opp+1)%3+1 }
+    };
+
+    let part1: i32 = input.lines().map(calc_score1).sum();
+    let part2: i32 = input.lines().map(calc_score2).sum();
+    let elapsed = Instant::now()-start;
+    println!("Day 2");
+    println!("  Part 1: {}", part1);
+    println!("  Part 2: {}", part2);
     println!("  Elapsed time: {:?}", elapsed);
 }
