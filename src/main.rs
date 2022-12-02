191aad166feb7719d7501a63da3424db0e2f4f8e
Tim Crossley <tcrossley@gmail.com>
2022-12-01 16:34:46 -0800

    Rewrite day1 to be more optimized, only store top 3

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
+use std::time::Instant;
+
 fn main() {
     day1();
 }
 
 pub fn day1() {
-    let input = std::fs::read_to_string("data/day1.txt").unwrap();
-    let mut elves = input.split("\r\n\r\n")
-        .map(|elf| {
-            elf.lines().map(|n| n.parse::<i32>().unwrap()).sum::<i32>()
+    let input = include_str!("../data/day1.txt");
+    let start = Instant::now();
+    let mut a = 0;
+    let mut b = 0;
+    let mut c = 0;
+    let mut current = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            if current > a { c=b; b=a; a=current; }
+            else if current > b { c=b; b=current; }
+            else if current > c { c=current; }
+            current = 0;
+        } else {
+            current += line.parse::<i32>().unwrap();
         }
-        )
-        .collect::<Vec<i32>>();
-    elves.sort();
-    
-    let len = elves.len();
-
-    println!("Part 1: {}", elves[len-1]);
-    println!("Part 2: {}", elves[len-1]+elves[len-2]+elves[len-3]);
+    }
+    let elapsed = Instant::now()-start;
+    println!("Day 1");
+    println!("  Part 1: {}", a);
+    println!("  Part 2: {}", a+b+c);
+    println!("  Elapsed time: {:?}", elapsed);
 }
