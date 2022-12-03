3b101f0b4d94c7e3567cd024569f0a8dd1f5decb
Tim Crossley <tcrossley@gmail.com>
2022-12-02 21:25:17 -0800

    Day 3 implemented

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 fn main() {
     day1();
     day2();
+    day3();
 }
 
 pub fn day1() {
@@ -47,6 +48,68 @@ fn day2() {
     }
     let elapsed = Instant::now()-start;
     println!("Day 2");
+    println!("  Part 1: {}", part1);
+    println!("  Part 2: {}", part2);
+    println!("  Elapsed time: {:?}", elapsed);
+}
+
+fn next_group<'a, I: Iterator<Item=&'a str>>(iter: &mut I) -> Option<(&'a str, &'a str, &'a str)> {
+    if let Some(a) = iter.next() {
+        if let Some(b) = iter.next() {
+            if let Some(c) = iter.next() {
+                return Some((a, b, c));
+            }
+        }
+    }
+    None
+}
+
+fn day3() {
+    let start = Instant::now();
+    let input = include_str!("../data/day3.txt");
+    let count_items = |s: &str| {
+        let mut counts = [0;52];
+        for b in s.bytes() {
+            match b {
+                b'a'..=b'z' => counts[(b-b'a') as usize] += 1,
+                b'A'..=b'Z' => counts[(b-b'A'+26) as usize] += 1,
+                _ => {}
+            }
+        }
+        counts
+    };
+    let mut part1 = 0;
+    for line in input.lines() {
+        let (p1, p2) = line.split_at(line.len()/2);
+        let counts1 = count_items(p1);
+        let counts2 = count_items(p2);
+        for i in 0..counts1.len() {
+            if counts1[i] > 0 && counts2[i] > 0 {
+                part1 += i+1;
+                break;
+            }
+        }
+    }
+
+    // Part 2
+    let mut part2 = 0;
+    let mut lines = input.lines();
+    while let Some((a, b, c)) = next_group(&mut lines) {
+        let c1 = count_items(a);
+        let c2 = count_items(b);
+        let c3 = count_items(c);
+
+        for i in 0..c1.len() {
+            if c1[i] > 0 && c2[i] > 0 && c3[i] > 0 {
+                part2 += i+1;
+                break;
+            }
+        }
+    }
+
+
+    let elapsed = Instant::now()-start;
+    println!("Day 3");
     println!("  Part 1: {}", part1);
     println!("  Part 2: {}", part2);
     println!("  Elapsed time: {:?}", elapsed);
